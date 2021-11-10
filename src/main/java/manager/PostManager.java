e9a958146c7dc0e6f30ffe749c39f6e4feab1d84
Alexander Maletin <man.alex212@gmail.com>
2021-11-10 22:38:19 +0500

    Added PostManager

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,16 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(int ownerId, String domain, String query, boolean ownersOnly, byte count, int offset) {
+
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+
+    }
+}
